a38b4ddfb75908b33c2b2708bb290e89ef736422
ttttester102 <ttttester102@gmail.com>
2019-10-05 22:35:16 +0530

    Enable production build

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,9 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+    onlyAllowOrigins: [
+      'http://3.15.230.14'
+    ],
 
 
     /***************************************************************************
@@ -337,7 +336,7 @@ module.exports = {
   * this, just try deploying without setting it and see if it works.)       *
   *                                                                         *
   ***************************************************************************/
-  // port: 80,
+  port: 80,
 
 
 
